545731fe41395c249745645013ac00ee2b00f3f0
MrDoCa <80938620+MrDoCa@users.noreply.github.com>
2022-12-13 23:04:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,13 +5,14 @@
 
 import os
 import asyncio
+import random
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, ADVERT_TEXT, VIDS
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
@@ -93,7 +94,8 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
+        await message.reply_video(
+            video = random.choice(VIDS),
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
